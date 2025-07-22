f7d476cc05c0708e8d6edc62cc9e9ab7b6569e98
Sam Chung <samchungy@gmail.com>
2025-07-23 01:05:41 +1000

    fix bundle (#8)

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import preferZodNamespace from "./rules/prefer-zod-namespace";
 import { TSESLint } from "@typescript-eslint/utils";
 
 // Create the base plugin object
-const plugin = {
+const importZod = {
   meta: {
     name: "import-zod",
     version: "1.0.0",
@@ -16,12 +16,12 @@ const plugin = {
 };
 
 Object.assign(
-  plugin.configs as NonNullable<TSESLint.Linter.Plugin["configs"]>,
+  importZod.configs as NonNullable<TSESLint.Linter.Plugin["configs"]>,
   {
     recommended: [
       {
         plugins: {
-          "import-zod": plugin,
+          "import-zod": importZod,
         },
         rules: {
           "import-zod/prefer-zod-namespace": "error",
@@ -31,4 +31,4 @@ Object.assign(
   }
 );
 
-export = plugin;
+export = importZod;
